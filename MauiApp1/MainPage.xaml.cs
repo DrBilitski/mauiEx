8e66f2f4648d40938d52da253bc893bc417b05fb
Jim <>
2026-03-01 23:11:13 -0500

    example connection to azure sql

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MauiApp1;
+﻿using System.Collections.ObjectModel;
+namespace MauiApp1;
 
 public partial class MainPage : ContentPage
 {
@@ -7,8 +8,47 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+		// Test Azure SQL connection and fetch records on startup
+		TestAzureSqlConnection();
 	}
 
+	public ObservableCollection<DogRecord> Dogs { get; set; } = new();
+
+	private async void TestAzureSqlConnection()
+	{
+		string connectionString = "FILL_IN_YOUR_CONNECTION_STRING_HERE";
+		try
+		{
+			using (var connection = new Microsoft.Data.SqlClient.SqlConnection(connectionString))
+			{
+				await connection.OpenAsync();
+				var command = new Microsoft.Data.SqlClient.SqlCommand("SELECT TOP 20 ID, Dog FROM Dogs", connection);
+				using (var reader = await command.ExecuteReaderAsync())
+				{
+					Dogs.Clear();
+					while (await reader.ReadAsync())
+					{
+						Dogs.Add(new DogRecord
+						{
+							ID = reader.GetInt32(0),
+							Dog = reader.GetString(1)
+						});
+					}
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			await Application.Current.MainPage.DisplayAlert("Azure SQL Error", ex.Message, "OK");
+		}
+		DogsCollectionView.ItemsSource = Dogs;
+	}
+
+	public class DogRecord
+	{
+		public int ID { get; set; }
+		public string Dog { get; set; }
+	}
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
@@ -19,6 +59,8 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
+
+		TestAzureSqlConnection();
 	}
 }
 
